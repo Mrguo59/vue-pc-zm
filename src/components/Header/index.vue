8c4e828230111379a7fb3dcaefbbe1c145931bb1
Mrguo59 <1035063198@qq.com>
2020-11-27 16:09:14 +0800

    feat:配置路由组件

---
+++ src/components/Header/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    Header...
+    <p>Header child</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="less" scoped>
+.header {
+  background-color: hotpink;
+  p {
+    font-size: 30px;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// 安装插件
+Vue.use(VueRouter);
+
+import Home from '../views/Home';
+import Login from '../views/Login';
+import Register from '../views/Register';
+import Search from '../views/Search';
+
+export default new VueRouter({
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/login',
+			component: Login
+		},
+		{
+			path: '/register',
+			component: Register
+		},
+		{
+			path: '/search',
+			component: Search
+		}
+	]
+});
