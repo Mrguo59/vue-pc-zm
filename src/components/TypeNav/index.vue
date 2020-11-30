735ee21cc60ff2c0dfb2a878941b44fef78141ec
Mrguo59 <1035063198@qq.com>
2020-11-30 16:22:42 +0800

    feat: 优化移入移出功能及减少请求次数功能

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,12 +1,8 @@
 <template>
   <!-- 商品分类导航 -->
   <div class="type-nav">
-    <div
-      class="container"
-      @mouseenter="isSearchShow = true"
-      @mouseleave="isSearchShow = false"
-    >
-      <h2 class="all">全部商品分类</h2>
+    <div class="container" @mouseleave="isSearchShow = false">
+      <h2 class="all" @mouseenter="isSearchShow = true">全部商品分类</h2>
       <nav class="nav">
         <a href="###">服装城</a>
         <a href="###">美妆馆</a>
@@ -196,6 +192,8 @@ export default {
     },
   },
   mounted() {
+    //判断vuex里是否又数据，如果有数据，就不需要再发请求了
+    if (this.categoryList.length) return;
     // 调用vuex的action函数
     this.getCategoryList();
   },
