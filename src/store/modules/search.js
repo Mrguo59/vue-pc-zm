cd46234f9dff3892fda6582ba010c1973e67523d
Mrguo59 <1035063198@qq.com>
2020-12-01 18:48:45 +0800

    feat: 完成search组件动态数据渲染

---
+++ src/store/modules/search.js
@@ -0,0 +1,38 @@
+// search组件的vuex状态
+import { reqGetSearchList } from '@api/search';
+
+export default {
+	state: {
+		searchList: {
+			trademarkList: [], // 品牌数据
+			attrsList: [], // 品牌属性数据
+			goodsList: [] // 商品数据
+		}
+	},
+
+	getters: {
+		// 方便使用数据
+		trademarkList(state) {
+			return state.searchList.trademarkList;
+		},
+		attrsList(state) {
+			return state.searchList.attrsList;
+		},
+		goodsList(state) {
+			return state.searchList.goodsList;
+		}
+	},
+
+	actions: {
+		async getSearchList({ commit }, data = {}) {
+			const searchList = await reqGetSearchList(data);
+			commit('GET_SEARCH_LIST', searchList);
+		}
+	},
+
+	mutations: {
+		GET_SEARCH_LIST(state, searchList) {
+			state.searchList = searchList;
+		}
+	}
+};
